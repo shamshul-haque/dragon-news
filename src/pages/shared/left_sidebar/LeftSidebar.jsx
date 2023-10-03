80b5ea0a874547fb19e30edf3a324b33b437a96f
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-03 14:55:06 +0600

    load categories in left sidebar

--- src/pages/shared/left_sidebar/LeftSidebar.jsx
+++ src/pages/shared/left_sidebar/LeftSidebar.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSidebar = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      const res = await fetch("/categories.json");
+      const data = await res.json();
+      setCategories(data);
+    };
+    loadCategories();
+  }, []);
+
   return (
-    <div>
-      <h1>left side</h1>
+    <div className="space-y-3">
+      <h2 className="text-xl font-bold">All Categories</h2>
+      {categories.map((category) => (
+        <Link
+          to={`/category/${category.id}`}
+          key={category.id}
+          className="block ml-5"
+        >
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
